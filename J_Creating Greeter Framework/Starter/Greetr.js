d3ce8dda870afdd7324cd248585e409fe5a29866
Rafael Cruz <rafacteixeira@gmail.com>
2016-10-29 19:56:57 -0200

    Cria estrutura inicial. Define um contexto de execução fechado. expõe o framework no objeto global usando aliases e cuida da instanciação dos objetos, bem como define o prototype dos objetos instanciados, como o mesmo prototype do objeto do framework

--- J_Creating Greeter Framework/Starter/Greetr.js
+++ J_Creating Greeter Framework/Starter/Greetr.js
@@ -1,3 +1,23 @@
 (function(global, $) {
 
 })(window, jQuery);
+    var Greetr = function(firstname, lastname, language) {
+        return new Greetr.init(firstname, lastname, language);
+    }
+
+    Greetr.prototype = {};
+
+    Greetr.init = function(firstname, lastname, language) {
+
+        var self = this;
+        self.firstName = firstname || '';
+        self.lastName = lastname || '';
+        self.language = language || 'en';
+
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = global.G$ = Greetr;
+
+}(window, jQuery));
