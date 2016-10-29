a82cc7e2da4b538a8d2b48c9b9093d65c360b7cd
Rafael Cruz <rafacteixeira@gmail.com>
2016-10-29 21:01:00 -0200

    comentando o código

--- J_Creating Greeter Framework/Starter/Greetr.js
+++ J_Creating Greeter Framework/Starter/Greetr.js
@@ -1,26 +1,32 @@
 (function(global, $) {
 
+    //cria um novo objeto do framework, que é uma function
     var Greetr = function(firstname, lastname, language) {
         return new Greetr.init(firstname, lastname, language);
     }
 
+    //variavel que determina quais linguagens serão suportadas. não é público para o programador.
     var supportedLangs = ['en', 'es'];
 
+    //objeto que determina quais são os cumprimentos disponíveis. Não formais. Feito em objeto para poder acessar os atributos usando a notação [], já que temos a linguágem como atributo do objeto priincipal
     var greetings = {
         en: 'Hello',
         es: 'Hola'
     };
 
+    //objeto que determina quais são os cumprimentos disponíveis. Formais. Feito em objeto para poder acessar os atributos usando a notação [], já que temos a linguágem como atributo do objeto priincipal
     var formalGreetings = {
         en: 'Greetings',
         es: 'Saludos'
     };
 
+    //objeto que determina quais são as mensagens de log disponíveis. Feito em objeto para poder acessar os atributos usando a notação [], já que temos a linguágem como atributo do objeto priincipal
     var logMessages = {
         en: 'Logged in',
         es: 'Inició sessión'
     };
 
+    //Definição do prototype do Objeto Function que representa o framework
     Greetr.prototype = {
         fullName: function() {
             return this.firstName + ' ' + this.lastName;
@@ -69,6 +75,7 @@
         }
     };
 
+    //Function Constructor que representa o objeto que será retornado framework for invocado
     Greetr.init = function(firstname, lastname, language) {
 
         var self = this;
