4541e646a72de87e580a82b05b8d35936032e638
Rafael Cruz <rafacteixeira@gmail.com>
2016-10-29 21:44:43 -0200

    test app

--- J_Creating Greeter Framework/Starter/app.js
+++ J_Creating Greeter Framework/Starter/app.js
@@ -1,11 +1,17 @@
 var g = G$('John', 'Doe');
 console.log(g);
 
-g.greet(true)
-    .greet(false)
-    .setLang('es')
-    .greet(true)
-    .greet(false)
-    .setLang('pt_BR');
+// g.greet(true)
+//     .greet(false)
+//     .setLang('es')
+//     .greet(true)
+//     .greet(false)
+//     .setLang('pt_BR');
 
-g.setLang('en').HTMLGreet($('#greeting'), false);
+//g.setLang('en').HTMLGreet($('#greeting'), false);
+
+$('#login').click(function() {
+
+    $('#logindiv').hide();
+    var loginGrtr = G$('John', 'Doe', $('#lang').val()).HTMLGreet('#greeting', true).log();
+});
